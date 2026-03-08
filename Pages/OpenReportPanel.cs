abd9b9fef66feb8ea1573889b8b53950312ca1f0
Anen <whoyou1994j4920@gmail.com>
2026-03-08 23:34:37 +0500

    fix

--- Pages/OpenReportPanel.cs
+++ Pages/OpenReportPanel.cs
@@ -1,72 +1,72 @@
-﻿using BakeryApp.Models;
-using Microsoft.EntityFrameworkCore;
+﻿//using BakeryApp.Models;
+//using Microsoft.EntityFrameworkCore;
 
-namespace SP2.Pages
-{
-    public class OpenReportPanel : ContentPage
-    {
-        BakeryDbContext dbContext = new BakeryDbContext();
+//namespace SP2.Pages
+//{
+//    public class OpenReportPanel : ContentPage
+//    {
+//        BakeryDbContext dbContext = new BakeryDbContext();
 
-        public Panel panel;
+//        public Panel panel;
 
-        public VerticalStackLayout reportPanel;
-        public ScrollView scrollView;
+//        public VerticalStackLayout reportPanel;
+//        public ScrollView scrollView;
 
-        public OpenReportPanel(Panel panel)
-        {
-            this.panel = panel;
-        }
+//        public OpenReportPanel(Panel panel)
+//        {
+//            this.panel = panel;
+//        }
 
-        public void OpenPanel()
-        {
-            var closePanel = new Button { Text = "Назад", HorizontalOptions = LayoutOptions.Start, Margin = new Thickness(0, 0, 0, 10) };
-            scrollView = new ScrollView();
-            reportPanel = new VerticalStackLayout() { Children = { closePanel } };
-            scrollView.Content = reportPanel;
-            panel.Content = scrollView;
-            closePanel.Clicked += (_, _) => { panel.Content = panel.mainPanel; reportPanel.Clear(); };
+//        public void OpenPanel()
+//        {
+//            var closePanel = new Button { Text = "Назад", HorizontalOptions = LayoutOptions.Start, Margin = new Thickness(0, 0, 0, 10) };
+//            scrollView = new ScrollView();
+//            reportPanel = new VerticalStackLayout() { Children = { closePanel } };
+//            scrollView.Content = reportPanel;
+//            panel.Content = scrollView;
+//            closePanel.Clicked += (_, _) => { panel.Content = panel.mainPanel; reportPanel.Clear(); };
 
-            var ordersDetails = dbContext.Orders
-                .Include(o => o.Customer)
-                .Include(o => o.OrderItems)
-                .Include(o => o.Employee)
-                .ToList();
+//            var ordersDetails = dbContext.Orders
+//                .Include(o => o.Customer)
+//                .Include(o => o.OrderItems)
+//                .Include(o => o.Employee)
+//                .ToList();
 
-            foreach (var order in ordersDetails)
-            {
-                var employees = dbContext.Employees.FirstOrDefault(e => e.Id == order.EmployeeId);
-                var customer = dbContext.Customers.FirstOrDefault(c => c.Id ==  order.CustomerId);
-                var orderitem = dbContext.OrderItems.FirstOrDefault(o => o.OrderId == order.Id);
+//            foreach (var order in ordersDetails)
+//            {
+//                var employees = dbContext.Employees.FirstOrDefault(e => e.Id == order.EmployeeId);
+//                var customer = dbContext.Customers.FirstOrDefault(c => c.Id ==  order.CustomerId);
+//                var orderitem = dbContext.OrderItems.FirstOrDefault(o => o.OrderId == order.Id);
 
-                if (employees == null) continue;
-                if (customer == null) continue;
-                if (orderitem == null) continue;
+//                if (employees == null) continue;
+//                if (customer == null) continue;
+//                if (orderitem == null) continue;
 
-                var border = new Border
-                {
-                    BackgroundColor = Colors.Black,
-                    Padding = 10,
-                    Margin = new Thickness(5)
-                };
+//                var border = new Border
+//                {
+//                    BackgroundColor = Colors.Black,
+//                    Padding = 10,
+//                    Margin = new Thickness(5)
+//                };
 
-                var verticalStackLayout = new VerticalStackLayout();
+//                var verticalStackLayout = new VerticalStackLayout();
 
-                foreach (var item in order.OrderItems)
-                {
-                    var product = dbContext.Products.FirstOrDefault(c => c.Id == item.ProductId);
-                    if (product == null) continue;
+//                foreach (var item in order.OrderItems)
+//                {
+//                    var product = dbContext.Products.FirstOrDefault(c => c.Id == item.ProductId);
+//                    if (product == null) continue;
 
-                    var label = new Label
-                    {
-                        Text = $"{order.Id}. Продавец {employees.LastName} {employees.FirstName} продал {product.Name} покупателю {customer.FirstName} за {item.Price}₸ в количестве {orderitem.Quantity} шт. Дата: {order.OrderDate}"
-                    };
+//                    var label = new Label
+//                    {
+//                        Text = $"{order.Id}. Продавец {employees.LastName} {employees.FirstName} продал {product.Name} покупателю {customer.FirstName} за {item.Price}₸ в количестве {orderitem.Quantity} шт. Дата: {order.OrderDate}"
+//                    };
 
-                    verticalStackLayout.Children.Add(label);
-                }
+//                    verticalStackLayout.Children.Add(label);
+//                }
 
-                border.Content = verticalStackLayout;
-                reportPanel.Children.Add(border);
-            }
-        }
-    }
-}
+//                border.Content = verticalStackLayout;
+//                reportPanel.Children.Add(border);
+//            }
+//        }
+//    }
+//}
--- Pages/OpenUserPanel.cs
+++ Pages/OpenUserPanel.cs
@@ -1,36 +1,36 @@
-﻿using BakeryApp.Models;
+﻿//using BakeryApp.Models;
 
-namespace SP2.Pages
-{
-    public class OpenUserPanel
-    {
-        BakeryDbContext dbContext = new BakeryDbContext();
-        public Panel panel;
-        VerticalStackLayout userPanel;
+//namespace SP2.Pages
+//{
+//    public class OpenUserPanel
+//    {
+//        BakeryDbContext dbContext = new BakeryDbContext();
+//        public Panel panel;
+//        VerticalStackLayout userPanel;
 
-        public OpenUserPanel(Panel panel)
-        {
-            this.panel = panel;
-        }
+//        public OpenUserPanel(Panel panel)
+//        {
+//            this.panel = panel;
+//        }
 
-        public void OpenPanel()
-        {
-            var closeButton = new Button
-            {
-                Text = "Назад",
-                HorizontalOptions = LayoutOptions.Start,
-                Margin = new Thickness(0, 0, 0, 10)
-            };
+//        public void OpenPanel()
+//        {
+//            var closeButton = new Button
+//            {
+//                Text = "Назад",
+//                HorizontalOptions = LayoutOptions.Start,
+//                Margin = new Thickness(0, 0, 0, 10)
+//            };
 
-            closeButton.Clicked += (_, _) => { panel.Content = panel.mainPanel; userPanel.Clear(); };
+//            closeButton.Clicked += (_, _) => { panel.Content = panel.mainPanel; userPanel.Clear(); };
 
-            userPanel = new VerticalStackLayout { Children = { closeButton } };
-            panel.Content = userPanel;
+//            userPanel = new VerticalStackLayout { Children = { closeButton } };
+//            panel.Content = userPanel;
 
-            foreach (var users in dbContext.Employees)
-            {
+//            foreach (var users in dbContext.Employees)
+//            {
 
-            }
-        }
-    }
-}
+//            }
+//        }
+//    }
+//}
